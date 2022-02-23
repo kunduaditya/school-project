2435403265f5fff05f0b070f9f978b1c27efe99c
kunduaditya <kunduaditya2006@gmail.com>
2022-02-23 15:43:25 +0530

    Update defaults.js

--- defaults.js
+++ defaults.js
@@ -1,6 +1,6 @@
 const template = (head, title, body) => {
   const link = window.location.href.split("/").pop();
-  const file = link.replace(/%20/g, " ");
+  let file = link.replace(/%20/g, " ");
   // Add .html to the end of the link if it's not there
   if (!file.endsWith(".html")) {
     file += ".html";
